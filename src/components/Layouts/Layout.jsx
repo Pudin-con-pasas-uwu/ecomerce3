2a5d8e0ea730b2e5a420b776bfacac665321502d
andres_realpozo <bollofrances@gmail.com>
2023-03-18 12:09:58 -0600

    awda
    awda

--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = (props) => {
   return (
     <div>
       <Head>
-        <title>this is a boostrap test</title>
+        <title>ecommerce</title>
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
