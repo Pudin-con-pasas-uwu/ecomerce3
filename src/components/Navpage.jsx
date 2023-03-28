bac9abe1356577a0210d00f056b70db27b72e181
Pudin_con_pasas_UwU <bollofrances@gmail.com>
2023-03-28 22:31:30 +0200

    cambios de textos
    cambios de textos

--- src/components/Navpage.jsx
+++ src/components/Navpage.jsx
@@ -14,11 +14,11 @@ const Navpage = () => {
   <input className="menu-btn" type="checkbox" id="menu-btn" />
   <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
   <ul class="menu">
-    <li><Link href="/aboutpage" className="mt-4">ABOUT</Link></li> 
-    <li><Link href="/Contact_us" className="mt-4">CONTACT US</Link></li>
+    <li><Link href="/aboutpage" className="mt-4">Sobre Nosotros</Link></li> 
+    <li><Link href="/Contact_us" className="mt-4">Contactanos</Link></li>
     <li><Link href="/LoginPage" className="mt-4">LOGIN</Link></li>
     <li><Link href="/Productos" className="mt-4">Productos</Link></li>
-    <li><Link href="/cart" className="mt-4">Carrito</Link></li>
+    <li><Link href="/cart" className="mt-4">Tu Carrito</Link></li>
   </ul>
 </header>
 </div>
