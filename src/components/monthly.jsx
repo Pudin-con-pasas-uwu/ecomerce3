b8cfcfee3dca6930ea36ac804f5e8705c46037b0
Moana Rivera Padilla <moana.padilla.0@gmail.com>
2023-04-16 16:48:16 -0500

    Cambios responsivo
    Se hicieron cambios en el responsivo del Monthly y Footer.
    
    Persiste un problema en el input del monthly, se intento cambiar pero no se pudo, se cree que es del servidor o intenet

--- src/components/monthly.jsx
+++ src/components/monthly.jsx
@@ -16,8 +16,8 @@ const Monthly  = () => {
                         <p className={styles.news}>¡Suscribete y unete a nuestra aldea, donde encontrarás los mejores productos a los mejores precios!</p>
                     </div>
                     <div className="col">
-                        <form className={styles.email} action="https://app.us14.list-manage.com/subscribe/post?u=bf44b77a96eca4a9f0d57cc98&amp;id=95d43bddc6&amp;f_id=00d698e0f0" method="post">
-                            <input type="email" className={styles.enteremail} name= "EMAIL" class="required email" id="mce-EMAIL" placeholder="Ingresa tu correo electrónico" required/>
+                        <form  action="https://app.us14.list-manage.com/subscribe/post?u=bf44b77a96eca4a9f0d57cc98&amp;id=95d43bddc6&amp;f_id=00d698e0f0" method="post">
+                            <input type="email" className={styles.eemail} name= "EMAIL" class="required email" id="mce-EMAIL" placeholder="Ingresa tu correo electrónico" required/>
                             <button type="submit" className={styles.submit} value="Subscribe" name="subscribe" id="mc-embedded-subscribe">ENVIAR</button>
                         </form>
                     </div>
